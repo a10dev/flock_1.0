1119e889e5c67187ac1d53c77920b3efb5c685d3
a10dev <burluckij_s_a@mail.ru>
2022-06-20 00:37:26 +0300

    FLocker sources. version 1.0

---
+++ set_environment.bat
@@ -0,0 +1,5 @@
+setX DG_SIGN_CERT_PASS "cert_sign_passw0rd" /M
+setX DG_SIGN_CERT "D:\signing\certs\cert_nochain_burlutsky.pfx" /M
+setX DG_CROSS_CERT "D:\signing\certs\DigiCert Assured ID Root CA.crt" /M
+
+pause
